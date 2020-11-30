d3adffd1a83eab8b53a9b5f238b36737933043aa
Ole <olelochen@gmail.com>
2020-11-30 15:09:35 +0100

    forside har funksjonell mobil nav

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function moveIntro() {
+  if(document.getElementById("menu_show").checked){
+    document.getElementById("main").style.marginTop = "17vh";
+  }else {
+    document.getElementById("main").style.marginTop = "2vh";
+  }
+
+}
