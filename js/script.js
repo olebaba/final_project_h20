2d25dbc8607b55d870270f0e82698c2afe9c0fa2
Ole <olelochen@gmail.com>
2020-11-30 15:12:44 +0100

    Alle sider har mobil navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function moveIntro() {
   if(document.getElementById("menu_show").checked){
-    document.getElementById("main").style.marginTop = "17vh";
+    document.getElementsByClassName("main")[0].style.marginTop = "17vh";
   }else {
-    document.getElementById("main").style.marginTop = "2vh";
+    document.getElementsByClassName("main")[0].style.marginTop = "2vh";
   }
 
 }
